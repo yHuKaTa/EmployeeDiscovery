b7e800aeff214b3c3cf23f60304acf11a610d0e1
Dimitar Enev <dreindead@abv.bg>
2023-12-17 18:33:54 +0200

    Create and config repository

---
+++ src/main/java/com/sirma/exam/repositories/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.sirma.exam.repositories;
+
+import com.sirma.exam.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/main/java/com/sirma/exam/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.sirma.exam.repositories;
+
+import com.sirma.exam.models.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
